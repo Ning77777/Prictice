9fcf2733c418b38a8c9e887c3d127eb2fef28c2f
Bruce <lshlsd@live.cn>
2016-07-03 09:21:00 +0800

    add P559-Basket practice

---
+++ P559-Basket/quote.h
@@ -0,0 +1,44 @@
+#ifndef QUOTE_H
+#define QUOTE_H
+
+#include <string>
+#include <iostream>
+
+class Quote{
+    friend bool operator !=(const Quote&, const Quote&);
+public:
+    Quote()=default;
+    Quote(const std::string& b, double p) : bookNo(b), price(p) {}
+    Quote(const Quote& q) : bookNo(q.bookNo), price(q.price) {}
+    Quote(const Quote&& q) noexcept : bookNo(std::move(q.bookNo)), price(std::move(q.price)) {}
+    Quote& operator =(const Quote& rhs){
+        if(*this!=rhs){
+            bookNo=rhs.bookNo;
+            price=rhs.price;
+        }
+        return *this;
+    }
+    Quote& operator =(Quote&& rhs) noexcept{
+        if(*this!=rhs){
+            bookNo=std::move(rhs.bookNo);
+            price=std::move(rhs.price);
+        }
+        return *this;
+    }
+    virtual Quote* clone() const & {return new Quote(*this);}
+    virtual Quote* clone() && {return new Quote(std::move(*this));}
+    virtual ~Quote()=default;
+private:
+    std::string bookNo;
+protected:
+    double price = 10.0;
+};
+
+bool inline
+operator != (const Quote& lhs, const Quote& rhs){
+    return lhs.bookNo != rhs.bookNo && lhs.price != rhs.price;
+}
+
+double printTotal(std::ostream& os, const Quote& item, size_t n);
+
+#endif
